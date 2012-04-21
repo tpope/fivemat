eb5741880f12082986e91fe4fa541254274d0b84
Chris Griego <cgriego@gmail.com>
2012-04-21 18:25:26 -0500

    Support RSpec 1

--- lib/fivemat.rb
+++ lib/fivemat.rb
@@ -2,11 +2,13 @@ module Fivemat
   autoload :Cucumber, 'fivemat/cucumber'
   autoload :MiniTest, 'fivemat/minitest/unit'
   autoload :RSpec, 'fivemat/rspec'
+  autoload :Spec, 'fivemat/spec'
 
   def self.new(*args)
     case args.size
     when 0 then MiniTest::Unit
     when 1 then RSpec
+    when 2 then Spec
     when 3 then Cucumber
     else
       raise ArgumentError
